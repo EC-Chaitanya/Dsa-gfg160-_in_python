965400d3ec1da4bf22048b572ddef72557c0890d
Chaitnaya Singh <159553178+EC-Chaitanya@users.noreply.github.com>
2025-04-30 17:58:32 +0530

    Add files via upload

---
+++ day24.py
@@ -0,0 +1,36 @@
+class Solution:
+	def mergeOverlap(self, arr):
+		#Code here
+        arr.sort()
+        merge = []
+        for i, j in arr:
+             if merge and  i <= merge[-1][1]:
+                 merge[-1][1] = max(merge[-1][-1], j)
+             else:
+                merge.append([i, j])
+        return merge
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+    T = int(input())
+    for i in range(T):
+        n = int(input())
+        # a = list(map(int, input().strip().split()))
+        arr = []
+        # j = 0
+        for i in range(n):
+            a = list(map(int, input().strip().split()))
+            x = a[0]
+            # j += 1
+            y = a[1]
+            # j += 1
+            arr.append([x, y])
+        obj = Solution()
+        ans = obj.mergeOverlap(arr)
+        for i in ans:
+            for j in i:
+                print(j, end=" ")
+        print()
+
+# } Driver Code Ends
