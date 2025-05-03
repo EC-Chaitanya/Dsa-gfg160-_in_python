34695c1d987b61da3c24fc31de883f3ff81d9fae
Chaitnaya Singh <159553178+EC-Chaitanya@users.noreply.github.com>
2025-05-03 21:28:20 +0530

    Add files via upload

---
+++ day31.py
@@ -0,0 +1,45 @@
+
+class Solution:   
+    def peakElement(self,arr):
+        # Code here
+        n = len(arr)
+        for i in range(n):
+            if(i == 0 or arr[i] > arr[i-1]) and (i == n -1 or arr[i] > arr[i + 1]):
+                return i
+        retur -1
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    t = int(input())  # Read number of test cases
+    for _ in range(t):
+        # Read input and split it into a list of integers
+        arr = list(map(int, input().split()))
+        # Create a Solution object and calculate the result
+
+        index = Solution().peakElement(arr)
+        n = len(arr)
+        flag = False
+        if index < 0 or index >= n:
+            flag = False
+        else:
+            if index == 0 and n == 1:
+                flag = True
+            elif index == 0 and arr[index] > arr[index + 1]:
+                flag = True
+            elif index == n - 1 and arr[index] > arr[index - 1]:
+                flag = True
+            elif index > 0 and index < n - 1 and arr[
+                    index - 1] < arr[index] and arr[index] > arr[index + 1]:
+                flag = True
+            else:
+                flag = False
+
+        if flag:
+            print("true")
+        else:
+            print("false")
+        print("~")
+
+# } Driver Code Ends
